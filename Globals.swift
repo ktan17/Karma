a8f3191900d093ffc40395faa821374e9d68ef8d
ktan17 <ktan17@ucla.edu>
2017-04-01 14:12:35 -0700

    database?????

---
+++ Globals.swift
@@ -0,0 +1,10 @@
+//
+//  Globals.swift
+//  Karma
+//
+//  Created by Kevin Tan on 4/1/17.
+//  Copyright © 2017 Green Bruins. All rights reserved.
+//
+
+import UIKit
+
--- Karma/AppDelegate.swift
+++ Karma/AppDelegate.swift
@@ -5,7 +5,7 @@
 //  Created by Kevin Tan on 4/1/17.
 //  Copyright © 2017 Green Bruins. All rights reserved.
 //
-// TEST COMMIT
+
 import UIKit
 import CoreData
 
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        loadDataFromCSV(file: "trash-data")
+        
         return true
     }
 
@@ -40,6 +43,115 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
+    }
+    
+    @available(iOS 10.0, *)
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+         */
+        let container = NSPersistentContainer(name: "Database")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    func loadDataFromCSV(file: String) {
+        
+        // Load file
+        guard let filepath = Bundle.main.path(forResource: file, ofType: "csv") else {
+            print("Failed to open database file")
+            exit(1)
+        }
+        
+        // Get contents
+        do {
+            let contents = try String(contentsOfFile: filepath)
+            createDatabase(data: contents)
+        } catch {
+            print("File Read Error for file \(filepath)")
+            return
+        }
+        
+    }
+    
+    func createDatabase(data: String) {
+        
+        let delimiter = ","
+        let lines: [String] = data.components(separatedBy: NSCharacterSet.newlines) as [String]
+        var items = [(name: String, trashType: Int, comment: String)]()
+        
+        var managedObjectContext: NSManagedObjectContext?
+        
+        if #available(iOS 10.0, *) {
+            managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        } else {
+            // iOS 9.0 and below - however you were previously handling it
+            guard let modelURL = Bundle.main.url(forResource: "Model", withExtension:"momd") else {
+                fatalError("Error loading model from bundle")
+            }
+            guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
+                fatalError("Error initializing mom from: \(modelURL)")
+            }
+            let psc = NSPersistentStoreCoordinator(managedObjectModel: mom)
+            managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+            let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+            let docURL = urls[urls.endIndex-1]
+            let storeURL = docURL.appendingPathComponent("Model.sqlite")
+            do {
+                try psc.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+            } catch {
+                fatalError("Error migrating store: \(error)")
+            }
+            
+        }
+        
+        for line in lines {
+            
+            var values: [String]
+            values = line.components(separatedBy: delimiter)
+        
+            let nextItem = (values[0], Int(values[1])!, values[2])
+            items.append(nextItem)
+            
+        }
+        
+        for item in items {
+            
+            let nextEntry = NSEntityDescription.insertNewObject(forEntityName: "TrashItem", into: managedObjectContext!) as! TrashItem
+            nextEntry.name = item.name
+            nextEntry.type = Int16(item.trashType)
+            nextEntry.comment = item.comment
+
+            do {
+                
+                try managedObjectContext?.save()
+                
+            } catch {
+                
+                print("insert error: \(error.localizedDescription)")
+                
+            }
+            
+        }
+        
     }
 
 }
