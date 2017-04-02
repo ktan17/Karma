f089b8a338d3ae8f8f294181177963cc7e4fb5db
ktan17 <ktan17@ucla.edu>
2017-04-01 23:00:51 -0700

    asdasd

--- Karma/AppDelegate.swift
+++ Karma/AppDelegate.swift
@@ -33,7 +33,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         let defaults = UserDefaults.standard
-        defaults.set(false, forKey: "isPreloaded")
         let isPreloaded = defaults.bool(forKey: "isPreloaded")
         if !isPreloaded {
             removeData()
